2a31f299da76726cbeab42dc1123908a9baa18a5
Gabriel Boscaratto <104937145+GabrielBoscaratto@users.noreply.github.com>
2024-07-07 20:10:52 -0300

    Initial commit

---
+++ src/main/java/com/example/Menu/controller/FoodController.java
@@ -0,0 +1,34 @@
+package com.example.Menu.controller;
+
+import com.example.Menu.food.Food;
+import com.example.Menu.food.FoodRepository;
+import com.example.Menu.food.FoodRequestDTO;
+import com.example.Menu.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+
+        return foodList;
+    }
+}
---
+++ src/main/java/com/example/Menu/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.Menu.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+}
